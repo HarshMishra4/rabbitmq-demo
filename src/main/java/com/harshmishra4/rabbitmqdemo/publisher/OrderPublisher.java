1a61a3bcc55eb315466ac2bc05ea46161efdc6ea
Mishra Harsh Sanjay <34826453+HarshMishra4@users.noreply.github.com>
2024-02-03 21:00:46 +0530

    Removed unwanted codes and comments.

--- src/main/java/com/harshmishra4/rabbitmqdemo/publisher/OrderPublisher.java
+++ src/main/java/com/harshmishra4/rabbitmqdemo/publisher/OrderPublisher.java
@@ -20,16 +20,6 @@ public class OrderPublisher {
         this.template = template;
     }
 
-//    @PostMapping("/{restaurantName}")
-//    public String bookOrder(@RequestBody Order order, @PathVariable String restaurantName) {
-//        order.setOrderId(UUID.randomUUID().toString());
-////        restaurant service
-////        payment service
-//        OrderStatus status = new OrderStatus(order, "PROCESS", "Order placed in : " + restaurantName);
-////        Adding message to message-queue configured with exchange
-//        template.convertAndSend(MessageConfig.EXCHANGE, MessageConfig.ROUTING_KEY, status);
-//        return "Success";
-//    }
 
     @PostMapping("/notify")
     public void bookOrder(@RequestBody MessageDetails details) {
@@ -38,12 +28,3 @@ public class OrderPublisher {
     }
 
 }
-
-// Comparison B/W Kafka & RabbitMQ -> https://www.youtube.com/watch?v=_5mu7lZz5X4
-
-//C:\Program Files\RabbitMQ Server\rabbitmq_server-3.12.10\sbin
-//rabbitmq-plugins enable rabbitmq_management
-//rabbitmq-server.bat start
-// If one is running RabbitMQ via rabbitmq-server.bat -detached,
-// the command for shutting it down is rabbitmqctl.bat shutdown
-
